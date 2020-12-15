ff9f2b9933bcc4807f0f0382ef660cc9deb16e07
jiekeliu <3073474788@qq.com>
2020-12-15 17:05:39 +0800

    up in 2020/12/15 17.05

--- src/main/java/com/jiekeliu/controller/IndexData.java
+++ src/main/java/com/jiekeliu/controller/IndexData.java
@@ -7,7 +7,10 @@ import com.jiekeliu.service.StractureServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther: jiekeliu
@@ -28,6 +31,7 @@ public class IndexData {
 
 
     @GetMapping("/stracture")
+    @CrossOrigin
     public List<Stracture> getStacture(){
 
         List<Stracture> all_stacture = stractureService.getAll();
@@ -94,12 +98,53 @@ public class IndexData {
 
 
     @GetMapping("/catalogue")
+    @CrossOrigin(origins = "*")
     public List<Catalogue> getCatalogue(){
 
         List<Catalogue> all_catalogue = catalogueService.getAll();
         System.out.println(all_catalogue);
 
         return all_catalogue;
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/vue-admin-template/user/login")
+    public Map login() {
+//        {"code":20000,"data":{"token":"admin-token"}}
+        HashMap<String, Object> response = new HashMap<>();
+        HashMap<String, Object> responseData = new HashMap<>();
+        responseData.put("token","admin-token");
+        response.put("code",20000);
+        response.put("data",responseData);
+        return response;
+    }
+
+//    {"code":20000,"data":{"roles":["admin"],"introduction":"I am a super administrator","avatar":"https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif","name":"Super Admin"}}
+    @CrossOrigin(origins = "*")
+    @GetMapping("/info")
+    public Map info() {
+        String[] ar = new String[1];
+        ar[0] = "admin";
+        HashMap<String, Object> responseInfo = new HashMap<>();
+        HashMap<String, Object> responseData = new HashMap<>();
+        responseData.put("roles",ar);
+        responseData.put("name","Super admin");
+        responseData.put("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        responseInfo.put("code",20000);
+        responseInfo.put("introduction","I am a super administrator");
+        responseInfo.put("data",responseData);
+        return responseInfo;
+    }
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/vue-admin-template/user/logout")
+    public Map logout() {
+//      {"code":20000,"data":"success"}
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("code",20000);
+        response.put("data","success");
+        return response;
     }
 
 }
