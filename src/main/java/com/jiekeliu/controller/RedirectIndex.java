c7cd042643bd3c3c07f8958a137eb1ece9712c39
jiekeliu <3073474788@qq.com>
2021-01-04 19:04:03 +0800

    up in 2021/1/4

---
+++ src/main/java/com/jiekeliu/controller/RedirectIndex.java
@@ -0,0 +1,23 @@
+package com.jiekeliu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @Auther: jiekeliu
+ * @Date: 2021/1/4 0004 18:05
+ * @Description:首页重定向控制器
+ */
+
+@RequestMapping
+@Controller
+public class RedirectIndex {
+    @GetMapping("/index")
+    @CrossOrigin(origins = "*")
+    public String indexRedirect(){
+        return "/simple/index.html";
+    }
+
+}
